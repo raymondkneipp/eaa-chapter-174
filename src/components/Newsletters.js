bdc349c8de725c20696b5bd99d50b07969b3e1a1
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2021-09-28 19:20:14 +0300

    Added 2021 flyer

--- src/components/Newsletters.js
+++ src/components/Newsletters.js
@@ -21,150 +21,172 @@ import newsletter10_19 from "../newsletters/2019/10-19-tailSpinners.pdf";
 
 import sworfi2020 from "../newsletters/2020/SWORFI 2020 Flyer.pdf";
 
+import sworfi2021 from "../newsletters/2021/SWORFI 2021 Flyer.pdf";
+
 const Newsletters = () => {
-	return (
-		<>
-			<h3 className="m-0">Newsletters</h3>
+  return (
+    <>
+      <h3 className="m-0">Newsletters</h3>
 
-			<div id="accordion">
-				<div
-					className="accordion-header h5"
-					id="heading2020"
-					data-toggle="collapse"
-					data-target="#collapse2020"
-				>
-					2020 <i className="fas fa-caret-down float-right" />
-				</div>
+      <div id="accordion">
+        <div
+          className="accordion-header h5"
+          id="heading2021"
+          data-toggle="collapse"
+          data-target="#collapse2021"
+        >
+          2021 <i className="fas fa-caret-down float-right" />
+        </div>
 
-				<div
-					id="collapse2020"
-					className="collapse show accordion-body"
-					data-parent="#accordion"
-				>
-					<p>
-						<a href={sworfi2020} target="_blank" rel="noopener noreferrer">
-							SWORFI
-						</a>
-					</p>
-				</div>
+        <div
+          id="collapse2021"
+          className="collapse show accordion-body"
+          data-parent="#accordion"
+        >
+          <p>
+            <a href={sworfi2021} target="_blank" rel="noopener noreferrer">
+              SWORFI
+            </a>
+          </p>
+        </div>
+        <div
+          className="accordion-header h5"
+          id="heading2020"
+          data-toggle="collapse"
+          data-target="#collapse2020"
+        >
+          2020 <i className="fas fa-caret-down float-right" />
+        </div>
 
-				<div
-					className="accordion-header h5"
-					id="headingOne"
-					data-toggle="collapse"
-					data-target="#collapseOne"
-				>
-					2019 <i className="fas fa-caret-down float-right" />
-				</div>
+        <div
+          id="collapse2020"
+          className="collapse show accordion-body"
+          data-parent="#accordion"
+        >
+          <p>
+            <a href={sworfi2020} target="_blank" rel="noopener noreferrer">
+              SWORFI
+            </a>
+          </p>
+        </div>
 
-				<div
-					id="collapseOne"
-					className="collapse show accordion-body"
-					data-parent="#accordion"
-				>
-					<p>
-						<a href={newsletter10_19} target="_blank" rel="noopener noreferrer">
-							October
-						</a>
-					</p>
-					<p>
-						<a href={newsletter9_19} target="_blank" rel="noopener noreferrer">
-							September
-						</a>
-					</p>
-					<p>
-						<a href={sworfi2019} target="_blank" rel="noopener noreferrer">
-							SWORFI
-						</a>
-					</p>
-					<p>
-						<a href={newsletter8_19} target="_blank" rel="noopener noreferrer">
-							August
-						</a>
-					</p>
-					<p>
-						<a href={newsletter7_19} target="_blank" rel="noopener noreferrer">
-							July
-						</a>
-					</p>
-					<p>
-						<a href={newsletter6_19} target="_blank" rel="noopener noreferrer">
-							June
-						</a>
-					</p>
-					<p>
-						<a href={newsletter2_19} target="_blank" rel="noopener noreferrer">
-							February
-						</a>
-					</p>
-					<p>
-						<a href={newsletter1_19} target="_blank" rel="noopener noreferrer">
-							January
-						</a>
-					</p>
-				</div>
-				<div
-					className="accordion-header h5"
-					id="headingTwo"
-					data-toggle="collapse"
-					data-target="#collapseTwo"
-				>
-					2018 <i className="fas fa-caret-down float-right" />
-				</div>
-				<div
-					id="collapseTwo"
-					className="collapse accordion-body"
-					data-parent="#accordion"
-				>
-					<p>
-						<a href={newsletter12_18} target="_blank" rel="noopener noreferrer">
-							December
-						</a>
-					</p>
-					<p>
-						<a href={newsletter11_18} target="_blank" rel="noopener noreferrer">
-							November
-						</a>
-					</p>
-					<p>
-						<a href={newsletter10_18} target="_blank" rel="noopener noreferrer">
-							October
-						</a>
-					</p>
-					<p>
-						<a href={newsletter7_18} target="_blank" rel="noopener noreferrer">
-							July
-						</a>
-					</p>
-					<p>
-						<a href={newsletter6_18} target="_blank" rel="noopener noreferrer">
-							June
-						</a>
-					</p>
-					<p>
-						<a href={newsletter5_18} target="_blank" rel="noopener noreferrer">
-							May
-						</a>
-					</p>
-					<p>
-						<a href={newsletter3_18} target="_blank" rel="noopener noreferrer">
-							March
-						</a>
-					</p>
-					<p>
-						<a href={newsletter2_18} target="_blank" rel="noopener noreferrer">
-							February
-						</a>
-					</p>
-					<p>
-						<a href={newsletter1_18} target="_blank" rel="noopener noreferrer">
-							January
-						</a>
-					</p>
-				</div>
-			</div>
-		</>
-	);
+        <div
+          className="accordion-header h5"
+          id="headingOne"
+          data-toggle="collapse"
+          data-target="#collapseOne"
+        >
+          2019 <i className="fas fa-caret-down float-right" />
+        </div>
+
+        <div
+          id="collapseOne"
+          className="collapse show accordion-body"
+          data-parent="#accordion"
+        >
+          <p>
+            <a href={newsletter10_19} target="_blank" rel="noopener noreferrer">
+              October
+            </a>
+          </p>
+          <p>
+            <a href={newsletter9_19} target="_blank" rel="noopener noreferrer">
+              September
+            </a>
+          </p>
+          <p>
+            <a href={sworfi2019} target="_blank" rel="noopener noreferrer">
+              SWORFI
+            </a>
+          </p>
+          <p>
+            <a href={newsletter8_19} target="_blank" rel="noopener noreferrer">
+              August
+            </a>
+          </p>
+          <p>
+            <a href={newsletter7_19} target="_blank" rel="noopener noreferrer">
+              July
+            </a>
+          </p>
+          <p>
+            <a href={newsletter6_19} target="_blank" rel="noopener noreferrer">
+              June
+            </a>
+          </p>
+          <p>
+            <a href={newsletter2_19} target="_blank" rel="noopener noreferrer">
+              February
+            </a>
+          </p>
+          <p>
+            <a href={newsletter1_19} target="_blank" rel="noopener noreferrer">
+              January
+            </a>
+          </p>
+        </div>
+        <div
+          className="accordion-header h5"
+          id="headingTwo"
+          data-toggle="collapse"
+          data-target="#collapseTwo"
+        >
+          2018 <i className="fas fa-caret-down float-right" />
+        </div>
+        <div
+          id="collapseTwo"
+          className="collapse accordion-body"
+          data-parent="#accordion"
+        >
+          <p>
+            <a href={newsletter12_18} target="_blank" rel="noopener noreferrer">
+              December
+            </a>
+          </p>
+          <p>
+            <a href={newsletter11_18} target="_blank" rel="noopener noreferrer">
+              November
+            </a>
+          </p>
+          <p>
+            <a href={newsletter10_18} target="_blank" rel="noopener noreferrer">
+              October
+            </a>
+          </p>
+          <p>
+            <a href={newsletter7_18} target="_blank" rel="noopener noreferrer">
+              July
+            </a>
+          </p>
+          <p>
+            <a href={newsletter6_18} target="_blank" rel="noopener noreferrer">
+              June
+            </a>
+          </p>
+          <p>
+            <a href={newsletter5_18} target="_blank" rel="noopener noreferrer">
+              May
+            </a>
+          </p>
+          <p>
+            <a href={newsletter3_18} target="_blank" rel="noopener noreferrer">
+              March
+            </a>
+          </p>
+          <p>
+            <a href={newsletter2_18} target="_blank" rel="noopener noreferrer">
+              February
+            </a>
+          </p>
+          <p>
+            <a href={newsletter1_18} target="_blank" rel="noopener noreferrer">
+              January
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Newsletters;
