ee5d241d81bc2889de362f18b8a476ffff8124bb
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-26 16:27:29 -0400

    Updated packages

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,41 +1,35 @@
 import React from "react";
 
 function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => {
-    return (images[item.replace("./", "")] = r(item));
-  });
-  return images;
+	let images = {};
+	r.keys().map((item, index) => {
+		return (images[item.replace("./", "")] = r(item));
+	});
+	return images;
 }
 
 const images = importAll(
-  require.context("../images/gallery/", false, /\.(png|jpe?g|svg)$/)
+	require.context("../images/gallery/", false, /\.(png|jpe?g|svg)$/)
 );
 
 const Gallery = () => {
-  console.log(images["1.jpg"].default);
+	return (
+		<div className="main">
+			<h3>Gallery</h3>
 
-  return (
-    <div className="main">
-      <h3>Gallery</h3>
-
-      <div className="row">
-        {Object.keys(images)
-          .reverse()
-          .map((key, index) => (
-            <div key={index} className="col-12 col-md-6 col-lg-4 my-3">
-              <a href={images[key].default}>
-                <img
-                  src={images[key].default}
-                  alt={key}
-                  className="img-fluid"
-                />
-              </a>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
+			<div className="row">
+				{Object.keys(images)
+					.reverse()
+					.map((key, index) => (
+						<div key={index} className="col-12 col-md-6 col-lg-4 my-3">
+							<a href={images[key]}>
+								<img src={images[key]} alt={key} className="img-fluid" />
+							</a>
+						</div>
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default Gallery;
